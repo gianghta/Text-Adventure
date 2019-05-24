967785c6cbe537e07a301217def67423241601d1
Giang Ta <tagh@ucmail.uc.edu>
2019-05-24 12:55:15 -0400

    Restructure project

---
+++ TextAdventureGame/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Text Adventure game
+ * by Giang Ta, May 23 2019
+ * 
+ * The game storyline and scenario
+ * is inspired by Goblin Slayer manga
+ * with a bit mix of Dungeon & Dragon setup
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TextAdventureGame
+{
+    enum Direction
+    {
+        North,
+        South,
+        West,
+        East
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameSession.StartGame();
+            Console.Read();
+        }
+    }
+}
---
+++ TextAdventureGame/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventureGame
+{
+    class Room
+    {
+        public Dictionary<Direction, Room> roomExits = new Dictionary<Direction, Room>();
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
